7f6de37c89ebfd0e3a2d757cea592ccab2683b68
Shikhar Yadav <shikharyadav645@gmail.com>
2025-06-18 15:01:32 +0530

    no chat selected design added

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import Messages from "./Messages";
 import HeaderName from "./HeaderName";
 import InputMessage from "./InputMessage";
-
+import NoChatSelected from "./NoChatSelected";
 const MessageContainer = () => {
+  const isChatSelected = true;
   return (
     <div className="md:min-w-[450px] flex flex-col flex-1">
-      <HeaderName />
-      <div className="divider mt-0"></div>
-      <div className="flex-1 overflow-auto px-2">
-        <Messages />
-      </div>
-      <InputMessage />
+      {isChatSelected ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          <HeaderName />
+          <div className="divider mt-0"></div>
+          <div className="flex-1 overflow-auto px-2">
+            <Messages />
+          </div>
+          <InputMessage />
+        </>
+      )}
     </div>
   );
 };
---
+++ frontend/src/components/messages/NoChatSelected.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NoChatSelected = () => {
+  return (
+    <div className="w-full h-full text-center flex flex-col justify-center items-center gap-3 ">
+      <p className="text-4xl font-extrabold w-full">👋Welcome</p>
+      <p className="text-xl font-extrabold w-full">No Chat Selected</p>
+    </div>
+  );
+};
+
+export default NoChatSelected;
